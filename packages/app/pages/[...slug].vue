eb9d86351b58dacb36786b75d23f16324ff6483f
Sandeep Ramgolam <sandeep@ramgolam.com>
2025-01-17 20:26:24 +0400

    feat(wip): attempt to render content

---
+++ packages/app/pages/[...slug].vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+console.log('Route params:', route.params.slug[0])
+
+const { data: pageContent } = await useAsyncData(() => 
+    queryCollection('content').all()
+)
+console.log('Page content:', pageContent)
+</script>
+
+<template>
+  <ContentRenderer v-if="pageContent" :value="pageContent" />
+</template>
